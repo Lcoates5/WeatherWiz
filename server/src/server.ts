abb673d6e9d6f0dd802a27fb42d153e8dc3c5ea8
Lauren Coates <lspassey@gmail.com>
2024-08-10 11:29:08 -0700

    serve static files of entire client dist folder, implemented middleware for parsing JSON

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,20 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import path from 'path';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-
+app.use(express.static('client/dist'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
