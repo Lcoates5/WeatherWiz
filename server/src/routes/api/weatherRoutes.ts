2bef569d406e30830242fbe70f882a126ec71156
Lauren Coates <lspassey@gmail.com>
2024-08-10 15:04:19 -0700

    created functions to get weather data from the cities and got search history

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,17 +1,25 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const { city } = req.body;
+  WeatherService.getWeatherForCity(city).then((data) => {
+    HistoryService.addCity(city);
+    res.json(data);
+  });
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  const history = await HistoryService.getCities();
+  res.json(history);
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {});
