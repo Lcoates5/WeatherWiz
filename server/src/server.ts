ab3651f5e4bc478b52ad6730dab8cb8b7061d3ef
Lauren Coates <lspassey@gmail.com>
2024-08-10 11:29:44 -0700

    implement middeware to connect routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use('/api',routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
